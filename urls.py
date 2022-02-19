7fc921e0546d14514e897136c253ead997ff280c
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-19 23:00:10 +0330

    Fix issue: only authenticated users should see profiles images

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-
 urlpatterns = [
     path('', include('core.field_values_suggestions.urls')),
+
 ]
--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,2 @@
 from .viewsets import ModelViewSet, OwnerListModelViewSetMixin, SetOwnerModelViewListMixin  # noqa
+from .media_view import MedialView  # noqa
